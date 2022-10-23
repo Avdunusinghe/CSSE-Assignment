961b4ad611a905e60fa3721107326e85820e6e65
Avdunusinghe <avdunusinghe@gmail.com>
2022-10-23 10:30:36 +0530

    Template Design Pattern Configuration

---
+++ src/core/application/template/EmployeeTemplate.java
@@ -0,0 +1,12 @@
+package core.application.template;
+
+public abstract class EmployeeTemplate {
+
+    abstract void employeeFromEXL();
+    abstract void employeeTableCreate();
+    abstract void employeeAdd();
+    abstract void employeeById(String employeeId);
+    abstract void employeeDelete(String employeeId);
+    abstract void employeeDisplay();
+
+}
